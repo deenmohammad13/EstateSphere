0a29d99aeebea74b3dd9cd8d622520447de440f9
Deen Mohammad <deen9721@gmail.com>
2025-01-04 00:38:38 -0800

    creating property card

--- frontend/src/app/Property/Property-card/property-card.component.ts
+++ frontend/src/app/Property/Property-card/property-card.component.ts
@@ -12,5 +12,12 @@ import { Component } from "@angular/core";
 )
 
 export class PropertyCardComponent{
+    
+Property : any ={
+    "Id": 1,
+    "Name" : "Birla House",
+    "Type" : "House",
+    "Price" : 12000
+}
 
 }
