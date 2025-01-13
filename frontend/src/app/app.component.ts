5a3de4d657c2874ebdc26acd6ebd7128faf07d13
Deen Mohammad <deen9721@gmail.com>
2025-01-13 12:56:34 +0600

    routing part 1

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,16 +1,32 @@
 import { Component, NgModule } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet,provideRouter } from '@angular/router';
 import { PropertyCardComponent } from './Property/Property-card/property-card.component';
 import { PropertyListComponent } from './Property/property-list/property-list.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AddPropertyComponent } from './Property/add-property/add-property.component';
+import { Route, RouterModule } from '@angular/router';
 
+const appRoutes : Route[] = [
+  {path : '', component : PropertyListComponent},
+  {path : 'add-property', component : AddPropertyComponent}
+
+]
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PropertyCardComponent, PropertyListComponent, NavBarComponent],
+  imports: [
+        RouterOutlet, 
+        PropertyCardComponent, 
+        PropertyListComponent, 
+        NavBarComponent, 
+        AddPropertyComponent,
+        RouterModule.forRoot(appRoutes)
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'] // Corrected typo (styleUrl -> styleUrls)
 })
 export class AppComponent {
   title = 'frontend';
 }
+
+
